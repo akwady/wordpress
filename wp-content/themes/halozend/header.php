28e72c687af33228573b14b5f9e9ac7f9d79b48d
Nguyen Vinh <thevinh1841996@gmail.com>
2018-01-18 10:27:46 +0700

    update new

--- wp-content/themes/halozend/header.php
+++ wp-content/themes/halozend/header.php
@@ -4,10 +4,7 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="icon" href="http://acehomevietnam.com/wp-content/uploads/2017/05/cropped-ACE-Home-logo-02-32x32.png" sizes="32x32">
     <title><?php wp_title( '', true, 'right' ); ?></title>
-
-    <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
     <title>[title]</title>
     <meta name=keywords content="[content]">
