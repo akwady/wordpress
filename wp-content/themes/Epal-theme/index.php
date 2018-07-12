030a6899c3554d0bf161485dc8eabc11e7a84b21
Nguyen Vinh <thevinh1841996@gmail.com>
2018-07-12 09:13:11 +0700

    7.12.2018

--- wp-content/themes/Epal-theme/index.php
+++ wp-content/themes/Epal-theme/index.php
@@ -5,3 +5,16 @@
 <!---->
 <!---->
 <!---->
+
+
+
+<!--lấy dữ liệu trong Advanced Custom Fields-->
+<?php //echo get_field('' , 'option') ?>
+
+
+<!--lấy dữ vòng lập trong Advanced Custom Fields-->
+<?php //while(have_rows('' , 'option')) : the_row() ?>
+<!---->
+<?php //echo get_sub_field('' , 'option') ?>
+<!---->
+<?php //endwhile; ?>
--- wp-content/themes/Epal-theme/tpl-contact.php
+++ wp-content/themes/Epal-theme/tpl-contact.php
@@ -10,6 +10,13 @@
     <div class="container">
         <div class="row">
             <!--Nội Dung Contact-->
+
+
+
+            <div class="col-md-6">
+<!--                shortcode Lấy Form plugin-->
+                <?php echo do_shortcode('[gravityform id=1 title=false description=false ajax=true tabindex=49]') ?>
+            </div>
         </div>
     </div>
 </section>
