1fbd4d555d81f8613eb669279a55fd8a5ae0c8b2
unknown <thevinh1841996@gmail.com>
2018-11-07 16:46:24 +0700

    edit code

--- wp-content/themes/Epal-theme/inc/button-editer.php
+++ wp-content/themes/Epal-theme/inc/button-editer.php
@@ -1,49 +1,85 @@
 <?php
 
+
+
+
+
+
+
 function add_style_select_buttons( $buttons ) {
     array_unshift( $buttons, 'styleselect' );
     return $buttons;
 }
 add_filter( 'mce_buttons_2', 'add_style_select_buttons' );
-function my_custom_styles( $init_array ) {
 
+add_filter( 'tiny_mce_before_init', 'boxes_plugin_that_add_style_formats_options', 20 );
+
+function boxes_plugin_that_add_style_formats_options( $settings ){
+
+    /* Our Own Custom Options */
     $style_formats = array(
-        // These are the custom styles
         array(
-            'title' => 'Button 1',
-            'block' => 'span',
-            'classes' => 'btn btn-sm animated-button victoria-one',
-            'wrapper' => true,
+            'title'   => 'Button',
+            'items' => array(
+                array(
+                    'title' => 'Button 1',
+                    'block' => 'span',
+                    'classes' => 'btn btn-sm animated-button victoria-one',
+                    'wrapper' => true,
+                ),
+                array(
+                    'title' => 'Button 2',
+                    'block' => 'span',
+                    'classes' => 'btn btn-sm animated-button victoria-two',
+                    'wrapper' => true,
+                ),
+                array(
+                    'title' => 'Button 3',
+                    'block' => 'span',
+                    'classes' => 'btn btn-sm animated-button victoria-three',
+                    'wrapper' => true,
+                ),
+                array(
+                    'title' => 'Button 4',
+                    'block' => 'span',
+                    'classes' => 'btn btn-sm animated-button victoria-four',
+                    'wrapper' => true,
+                ),
+                array(
+                    'title' => 'Button 5',
+                    'block' => 'span',
+                    'classes' => 'btn btn-sm animated-button victoria-four',
+                    'wrapper' => true,
+                ),
+            ),
         ),
         array(
-            'title' => 'Button 2',
-            'block' => 'span',
-            'classes' => 'btn btn-sm animated-button victoria-two',
-            'wrapper' => true,
-        ),
-        array(
-            'title' => 'Button 3',
-            'block' => 'span',
-            'classes' => 'btn btn-sm animated-button victoria-three',
-            'wrapper' => true,
-        ),
-        array(
-            'title' => 'Button 4',
-            'block' => 'span',
-            'classes' => 'btn btn-sm animated-button victoria-four',
-            'wrapper' => true,
-        ),
-        array(
-            'title' => 'Button 5',
-            'block' => 'span',
-            'classes' => 'btn btn-sm animated-button victoria-four',
-            'wrapper' => true,
+            'title'   => 'Text',
+            'items' => array(
+                array(
+                    'title' => 'Text Uppercase',
+                    'block' => 'span',
+                    'classes' => 'Text-Uppercase',
+                    'wrapper' => true,
+                ),
+                array(
+                    'title' => 'Text Capitalize',
+                    'block' => 'span',
+                    'classes' => 'Text-Capitalize',
+                    'wrapper' => true,
+                ),
+                array(
+                    'title' => 'Text Lowercase',
+                    'block' => 'span',
+                    'classes' => 'Text-Lowercase',
+                    'wrapper' => true,
+                ),
+            ),
         ),
     );
-    // Insert the array, JSON ENCODED, into 'style_formats'
-    $init_array['style_formats'] = json_encode( $style_formats );
 
-    return $init_array;
-
+    /* Add it in tinymce config as json data */
+    $settings['style_formats'] = json_encode( $style_formats );
+    return $settings;
 }
-add_filter( 'tiny_mce_before_init', 'my_custom_styles' );
+
